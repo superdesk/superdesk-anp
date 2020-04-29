4efd7a9ec325519602b1cf1c3c31a98a153a752c
MarkLark86 <mark.pittaway@gmail.com>
2020-04-29 11:00:44 +1000

    Use develop branch for planning (#25)

--- client/anp/index.ts
+++ client/anp/index.ts
@@ -1,6 +1,14 @@
+import {startApp} from 'superdesk-core/scripts/index';
 import photo from './photo';
 import talpaVideo from './talpa-video'
+import planningExtension from 'superdesk-planning/client/planning-extension/dist/extension';
 
+setTimeout(() => {
+  startApp(
+      [planningExtension],
+      {},
+  );
+});
 
 export default angular.module('anp', [
   photo.name,
